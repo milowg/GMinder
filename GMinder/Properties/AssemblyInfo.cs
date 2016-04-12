6915ed63cc4d594768748db468b3a3f87caa2eea
Gil Milow <milowg@gmail.com>
2016-04-11 22:20:11 -0400

    Updated version for merge

--- GMinder/Properties/AssemblyInfo.cs
+++ GMinder/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.0.3")]
-[assembly: AssemblyFileVersion("1.4.0.3")]
+[assembly: AssemblyVersion("1.4.0.6")]
+[assembly: AssemblyFileVersion("1.4.0.6")]
